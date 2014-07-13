e4eb260c168c6d51a4b8f72098dc85b5130ec54b
Doug Kurth <doug.kurth@gmail.com>
2014-07-13 18:15:47 -0500

    Fleshing it out...

--- src/compiler.rs
+++ src/compiler.rs
@@ -3,8 +3,21 @@ use std::io::{File, BufferedReader};
 mod scanner;
 
 fn main() {
-	println!("Welcome to my compiler!");
 	let file = File::open(&Path::new("testdata/program1.c"));
 	let mut reader = BufferedReader::new(file);
-	scanner::getsymbol(& mut reader);
+	let mut s = scanner::Scanner::new();
+
+	let mut current_token: Option<scanner::Token> = None;
+
+	while current_token.is_none() || !current_token.unwrap().is_eof() {
+		match s.get_symbol(& mut reader) {
+			Err(_) => {
+				println!("Error encountered")
+				return;
+			}
+			Ok(token) => {
+				current_token = Some(token);
+			}
+		}
+	}
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,25 +1,112 @@
 use std::string::String;
 
+pub enum Token {
+	IdentifierToken(String),
+	LeftParenToken,
+	RightParenToken,
+	LeftCurlyToken,
+	RightCurlyToken,
+	IfToken,
+	ElseToken,
+	WhileToken,
+	AssignmentToken,
+	NumberToken(f64),
+	SemicolonToken,
+	EndOfFileToken,
+	ReturnToken
+}
 
-// This is the primary interface for the scanner, which will be invoked by the
-// parser.
-pub fn getsymbol(buffer: & mut Buffer) -> String {
-	match buffer.read_char() {
-		Err(_) => {}
-		Ok(char) => {
-			println!("{}", char);
+impl Token {
+	pub fn is_eof(&self) -> bool {
+		true
+	}
+}
+
+enum ScannerState {
+    CharState(char),
+    WordState(String),
+    NumberState(String)
+}
+
+impl ScannerState {
+	fn init(c: char) -> ScannerState {
+		if is_symbol_terminating_character(c) {
+			CharState(c)
+		} else { // TODO
+			NumberState(String::new())
 		}
 	}
 
-	String::from_str("yep")
+	fn process_char(&self, c: char) -> Result<Option<Token>, &'static str> {
+		// Update the state
+		// return token, if complete
+		Ok(None)
+	}
 }
 
-#[test]
-fn getsymbol_test() {
-	use std::io::{Buffer, File, BufferedReader};
-	
-	let file = File::open(&Path::new("testdata/program1.c"));
-	let mut reader = BufferedReader::new(file);
-	let symbol = getsymbol(& mut reader);
-	assert!(symbol == String::from_str("yep"));
+fn is_symbol_terminating_character(c: char) -> bool {
+	match c {
+		'(' => true,
+		')' => true,
+		'{' => true,
+		'}' => true,
+		'=' => true,
+		';' => true,
+		_  	=> false
+	}
+}
+
+pub struct Scanner {
+    current_char: Option<char>
+}
+
+impl Scanner {
+	pub fn new() -> Scanner {
+		Scanner { current_char: None }
+	}
+
+	// This is the primary interface for the scanner, which will be invoked by
+	// the parser.
+	pub fn get_symbol(& mut self, buffer: & mut Buffer) ->
+			Result<Token, &'static str> {
+
+		
+		let mut token: Option<Token> = None;
+
+		// If character not set, read first character
+		// TODO: Eliminate code duplication with a closure
+		if self.current_char.is_none() {
+			match buffer.read_char() {
+				Err(_) => {
+					return Err("Oops!");
+				}
+				Ok(c) => {
+					self.current_char = Some(c);
+				}
+			}
+		}
+
+		let mut state = ScannerState::init(self.current_char.unwrap());
+
+		while token.is_none() {
+			match state.process_char(self.current_char.unwrap()) {
+				Err(_) => { return Err("Oops!"); }
+				Ok(t) => { token = t; }
+			}
+
+			// Read the next character, if a token is not yet constructed
+			if token.is_none() {
+				match buffer.read_char() {
+					Err(_) => {
+						return Err("Oops!");
+					}
+					Ok(c) => {
+						self.current_char = Some(c);
+					}
+				}
+			}
+		}
+
+		Ok(token.unwrap())
+	}
 }
